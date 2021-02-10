bc5a6ba42767904027ccb026906a9cde6bd13003
Seth VanNiekerk <swvn10@gmail.com>
2021-02-10 13:21:54 -0500

    Implement View Components
    Re: #26
    Move the navbar to a viewcomponent, and add the global error/success message.

---
+++ KurbSide/ViewComponents/NavBarViewComponent.cs
@@ -0,0 +1,39 @@
+﻿using KurbSide.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace KurbSide.ViewComponents
+{
+    [ViewComponent(Name = "NavBar")]
+    public class NavBarViewComponent : ViewComponent
+    {
+        private readonly KSContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public NavBarViewComponent(KSContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        private Task<IdentityUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        private async Task<string> GetLoggedInEmailAsync()
+        {
+            var user = await GetCurrentUserAsync();
+            return user == null ? "" : user.Email;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewData["email"] =  await GetLoggedInEmailAsync();
+            return await Task.FromResult((IViewComponentResult)View("Default"));
+            //return await Task.FromResult((IViewComponentResult)View("Default", model));
+        }
+    }
+}
