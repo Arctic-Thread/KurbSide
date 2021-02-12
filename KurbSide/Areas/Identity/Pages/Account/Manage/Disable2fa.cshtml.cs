4ccd3e072cf9c9b0101c9f6721860fbe29b0108c
Liam <lderivers@gmail.com>
2021-02-11 20:49:46 -0500

    Lower case 2fa is cringe inducing

--- KurbSide/Areas/Identity/Pages/Account/Manage/Disable2fa.cshtml.cs
+++ KurbSide/Areas/Identity/Pages/Account/Manage/Disable2fa.cshtml.cs
@@ -55,8 +55,8 @@ namespace KurbSide.Areas.Identity.Pages.Account.Manage
                 throw new InvalidOperationException($"Unexpected error occurred disabling 2FA for user with ID '{_userManager.GetUserId(User)}'.");
             }
 
-            _logger.LogInformation("User with ID '{UserId}' has disabled 2fa.", _userManager.GetUserId(User));
-            StatusMessage = "2fa has been disabled. You can reenable 2fa when you setup an authenticator app";
+            _logger.LogInformation("User with ID '{UserId}' has disabled 2FA.", _userManager.GetUserId(User));
+            StatusMessage = "2FA has been disabled. You can re-enable 2FA when you setup an authenticator app";
             return RedirectToPage("./TwoFactorAuthentication");
         }
     }
--- KurbSide/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
+++ KurbSide/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
@@ -64,7 +64,7 @@ namespace KurbSide.Areas.Identity.Pages.Account.Manage
             }
 
             await _signInManager.ForgetTwoFactorClientAsync();
-            StatusMessage = "The current browser has been forgotten. When you login again from this browser you will be prompted for your 2fa code.";
+            StatusMessage = "The current browser has been forgotten. When you login again from this browser you will be prompted for your 2FA code.";
             return RedirectToPage();
         }
     }
