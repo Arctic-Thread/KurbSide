08f5783ab12505cebac7a8e7b283d6b1e432d270
Liam <lderivers@gmail.com>
2021-02-19 15:00:32 -0500

    Unit Test | UC02 - View Privacy Policy
    #64

--- KurbSideTest/GenericTests.cs
+++ KurbSideTest/GenericTests.cs
@@ -37,5 +37,34 @@ namespace KurbSideTest
             //Assert
             Assert.IsTrue(result);
         }
+
+        /// <summary>
+        /// UC02 - View Privacy Policy
+        /// Tests if the Privacy Policy Page loads
+        /// </summary>
+        [Order(2)]
+        [Test]
+        public void UC02_Generic_ViewPrivacyPlicy_ShouldPass()
+        {
+            // Arrange
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+
+            // Fields & Buttons
+            string footerPrivacyID = "footer-privacy";
+
+            // Titles
+            string homePageTitle = "Home Page - KurbSide";
+            string privacyPolicyPageTitle = "Privacy Policy - KurbSide";
+
+            //Act
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleContains(homePageTitle));
+            _driver.FindElement(By.Id(footerPrivacyID)).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleContains(privacyPolicyPageTitle));
+
+            var result = _driver.Title.Contains(privacyPolicyPageTitle);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
     }
 }
