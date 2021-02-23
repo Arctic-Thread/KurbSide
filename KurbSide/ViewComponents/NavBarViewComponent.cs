13b615baca9ef072ccbcfaa53c9d135d23578b57
Liam <lderivers@gmail.com>
2021-02-23 14:43:04 -0500

    Dynamic Business Status(Open/Closed) in the navigation bar

--- KurbSide/ViewComponents/NavBarViewComponent.cs
+++ KurbSide/ViewComponents/NavBarViewComponent.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -65,9 +66,47 @@ namespace KurbSide.ViewComponents
                     .FirstOrDefaultAsync();
 
                 TempData["loggedInBusiness"] = business;
+                TempData["openForBusiness"] = await OpenForBusiness();
             }
 
             return await Task.FromResult((IViewComponentResult)View("Default"));
+        }
+
+        public async Task<bool> OpenForBusiness()
+        {
+            var user = await GetCurrentUserAsync();
+            if (GetAccountType(user) == "business")
+            {
+                var business = await _context.Business
+                    .Where(b => b.AspNetId.Equals(user.Id))
+                    .FirstOrDefaultAsync();
+
+                var businessHours = await _context.BusinessHours
+                    .FirstOrDefaultAsync(b => b.BusinessId == business.BusinessId);
+
+                DayOfWeek dayOfWeek = DateTime.Now.DayOfWeek;
+
+                switch (dayOfWeek)
+                {
+                    case DayOfWeek.Sunday:
+                        return businessHours.SunOpen < DateTime.Now.TimeOfDay && DateTime.Now.TimeOfDay < businessHours.SunClose;
+                    case DayOfWeek.Monday:
+                        return businessHours.MonOpen < DateTime.Now.TimeOfDay && DateTime.Now.TimeOfDay < businessHours.MonClose;
+                    case DayOfWeek.Tuesday:
+                        return businessHours.TuesOpen < DateTime.Now.TimeOfDay && DateTime.Now.TimeOfDay < businessHours.TuesClose;
+                    case DayOfWeek.Wednesday:
+                        return businessHours.WedOpen < DateTime.Now.TimeOfDay && DateTime.Now.TimeOfDay < businessHours.WedClose;
+                    case DayOfWeek.Thursday:
+                        return businessHours.ThuOpen < DateTime.Now.TimeOfDay && DateTime.Now.TimeOfDay < businessHours.ThuClose;
+                    case DayOfWeek.Friday:
+                        return businessHours.FriOpen < DateTime.Now.TimeOfDay && DateTime.Now.TimeOfDay < businessHours.FriClose;
+                    case DayOfWeek.Saturday:
+                        return businessHours.SatOpen < DateTime.Now.TimeOfDay && DateTime.Now.TimeOfDay < businessHours.SatClose;
+                    default:
+                        return false;
+                }
+            }
+            return false;
         }
 
     }
