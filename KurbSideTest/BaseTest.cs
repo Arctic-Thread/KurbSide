61904ff360edf96d1160d8f7f9c722e6f65207e5
Liam <lderivers@gmail.com>
2021-02-23 17:33:07 -0500

    Login method for unit tests.
    KSUnitTestLogin(AccountType) allows you to log in to a specified account type in a unit test.
    Available account types: MEMBER, BUSINESS or TEST

--- KurbSideTest/BaseTest.cs
+++ KurbSideTest/BaseTest.cs
@@ -5,9 +5,29 @@ using System.Linq;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace KurbSideTest
 {
+    /// <summary>
+    /// Specifies which account type should be used. MEMBER, BUSINESS or TEST
+    /// </summary>
+    public enum AccountType
+    {
+        /// <summary>
+        /// A Member account: member@kurbsi.de - Note: Should be used when testing member functionality.
+        /// </summary>
+        MEMBER,
+        /// <summary>
+        /// A Business account: business@kurbsi.de - Note: Should be used when testing business functionality.
+        /// </summary>
+        BUSINESS,
+        /// <summary>
+        /// A Business account: test@kurbsi.de - Note: Should be used for anything that doesn't fall under business or member specifically.
+        /// </summary>
+        TEST
+    }
+
     [TestFixture]
     public partial class BaseTest
     {
@@ -59,6 +79,55 @@ namespace KurbSideTest
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        /// <summary>
+        /// Logs in to the specified account type for the unit test.
+        /// </summary>
+        /// <param name="accountType">The account type to be used in the unit test.</param>
+        public void KSUnitTestLogin(AccountType accountType)
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+
+            // Login Details
+            string loginEmail;
+            string loginPassword = "Password12345";
+
+            switch (accountType)
+            {
+                case AccountType.MEMBER:
+                    loginEmail = "member@kurbsi.de";
+                    break;
+                case AccountType.BUSINESS:
+                    loginEmail = "business@kurbsi.de";
+                    break;
+                case AccountType.TEST:
+                    loginEmail = "test@kurbsi.de";
+                    break;
+                default:
+                    throw new NotImplementedException("KurbSideTest.BaseTest.KSUnitTestLogin - Account Type Not Implemented");
+            }
+
+
+
+            // Fields & Buttons
+            string navbarLoginButtonID = "navbar-login";
+            string loginEmailFieldID = "Input_Email";
+            string loginPasswordFieldID = "Input_Password";
+            string loginButtonID = "login-button";
+
+            // Titles
+            string homePageTitle = "Home Page - KurbSide";
+            string loginPageTitle = "Log in - KurbSide";
+
+            //Act
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleContains(homePageTitle)); // Wait until home page is visible.
+            _driver.FindElement(By.Id(navbarLoginButtonID)).Click(); // Click login button in navbar.
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleContains(loginPageTitle)); // Wait until login page is visible
+            _driver.FindElement(By.Id(loginEmailFieldID)).SendKeys(loginEmail); // Send email to email field.
+            _driver.FindElement(By.Id(loginPasswordFieldID)).SendKeys(loginPassword); // Send password to password field.
+            _driver.FindElement(By.Id(loginButtonID)).Click(); // Click the login button.
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleContains(homePageTitle)); // Wait until home page is visible.
         }
     }
 }
