48eda93774f694290ecfe89a29da532709ae1755
Liam <lderivers@gmail.com>
2021-02-24 18:58:07 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/Arctic-Thread/Capstone into main""
    This reverts commit 5c931b7aeed918c0653925086d95a9737b066c3a.

--- KurbSide/Controllers/HomeController.cs
+++ KurbSide/Controllers/HomeController.cs
@@ -6,20 +6,36 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using KurbSide.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace KurbSide.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly KSContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, KSContext context, UserManager<IdentityUser> userManager)
         {
             _logger = logger;
+            _context = context;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var user = await GetCurrentUserAsync();
+            var accountType = GetAccountType(user);
+            if (accountType == "business")
+            {
+                return RedirectToAction("Index", "Business");
+            }
+            else if (accountType == "member")
+            {
+                //TODO Display member "home page", the store maybe? 
+                //return RedirectToAction("Index", "Member");
+            }
             return View();
         }
 
@@ -38,5 +54,34 @@ namespace KurbSide.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        #region CurrentUserUtils
+        //Current User Utils 1.0
+        public Task<IdentityUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        public async Task<string> GetLoggedInEmailAsync()
+        {
+            var user = await GetCurrentUserAsync();
+            return user == null ? "" : user.Email;
+        }
+
+        public string GetAccountType(IdentityUser? IUser)
+        {
+            if (IUser == null) return "";
+
+            bool hasBusiness = _context.Business.Where(b => b.AspNetId.Equals(IUser.Id)).Count() > 0;
+            bool hasMember = _context.Member.Where(b => b.AspNetId.Equals(IUser.Id)).Count() > 0;
+
+            /*
+             * HACK maybe make this an enum rather than a string to prevent future issues
+             *      actually nevermind because C# enums aren't like Java enums
+             *      for now keeping it standard to lowercase strings works :)
+             */
+            if (hasBusiness) return "business";
+            else if (hasMember) return "member";
+            else return "";
+        }
+        //End Current User Utils 1.0
+        #endregion
     }
 }
