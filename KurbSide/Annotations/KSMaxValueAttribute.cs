b5c5ddbca9465b729b4d936631fb05751c51e0ed
Liam <lderivers@gmail.com>
2021-02-24 22:12:06 -0500

    More Data Annotations. Min and Max value
    Doubles only (for now, who knows might need something in the future).

---
+++ KurbSide/Annotations/KSMaxValueAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KurbSide.Annotations
+{
+    /// <summary>
+    /// Checks if the <b>double</b> is less than or equal to the specified max value.
+    /// </summary>
+    public class KSMaxValueAttribute : ValidationAttribute
+    {
+        private readonly double _maxValue;
+
+        public KSMaxValueAttribute(double maxValue)
+        {
+            _maxValue = maxValue;
+        }
+
+        public override bool IsValid(object value)
+        {
+            return (double)value <= _maxValue;
+        }
+    }
+}
