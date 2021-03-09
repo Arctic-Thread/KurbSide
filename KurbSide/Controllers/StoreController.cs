bfd6dbdd3bdcc1fc85a9e8b8cd307269050198b6
Seth VanNiekerk <swvn10@gmail.com>
2021-03-09 09:59:16 -0500

    Create StoreController.cs

---
+++ KurbSide/Controllers/StoreController.cs
@@ -0,0 +1,32 @@
+﻿using KurbSide.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KurbSide.Controllers
+{
+    [Authorize]
+    public class StoreController : Controller
+    {
+        private readonly KSContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<Business> _logger;
+
+        public StoreController(KSContext context, UserManager<IdentityUser> userManager, ILogger<Business> logger)
+        {
+            _context = context;
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
