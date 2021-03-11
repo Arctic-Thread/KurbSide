5ad3496d7f3602168f9d22cb9081a99d520a2fb2
Liam <lderivers@gmail.com>
2021-03-10 19:15:37 -0500

    Made the uploading & resetting of images prettier on the add/edit page

---
+++ KurbSide/wwwroot/js/Business/ItemManagement/Add.js
@@ -0,0 +1,20 @@
+﻿window.onload = function () {
+    document.getElementById('resetImage').onclick = function () {
+        document.getElementById('itemImage').value = '';
+        document.getElementById('labelForItemImage').innerHTML = '';
+        document.getElementById('imagePreview').hidden = true;
+    }
+
+    document.getElementById('imagePreview').onerror = function () {
+        this.alt = 'Invalid Image Selected.'
+    }
+
+    document.getElementById('itemImage').onchange = function () {
+        document.getElementById('imagePreview').src = window.URL.createObjectURL(this.files[0])
+        document.getElementById('imagePreview').hidden = false;
+    }
+
+    $(document).on('change', '.custom-file-input', function (event) {
+        $(this).next('.custom-file-label').html(event.target.files[0].name);
+    })
+};
---
+++ KurbSide/wwwroot/js/Business/ItemManagement/Edit.js
@@ -0,0 +1,22 @@
+﻿window.onload = function () {
+    document.getElementById('resetImageEdit').onclick = function () {
+        document.getElementById('itemImageEdit').value = '';
+        document.getElementById('labelForItemImage').innerHTML = '';
+        document.getElementById('imagePreviewEdit').src = document.getElementById('originalLocation').value
+    }
+
+    document.getElementById('imagePreviewEdit').onerror = function () {
+        this.alt = 'Invalid Image Selected.'
+    }
+
+    document.getElementById('itemImageEdit').onchange = function () {
+        document.getElementById('imagePreviewEdit').src = window.URL.createObjectURL(this.files[0])
+        document.getElementById('imagePreviewEdit').hidden = false;
+    }
+
+    $('.custom-file input').change(function (e) {
+        if (e.target.files.length) {
+            $(this).next('.custom-file-label').html(e.target.files[0].name);
+        }
+    });
+};
