d2cf286711141c075434415e49843b968f454b22
Liam De Rivers <lderivers@gmail.com>
2021-04-02 15:54:52 -0400

    Discounts in orders
    Discounts still need to be implemented in checkout

--- KurbSide/Utilities/KSOrderUtilities.cs
+++ KurbSide/Utilities/KSOrderUtilities.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using KurbSide.Models;
 
@@ -17,5 +18,24 @@ namespace KurbSide.Utilities
         /// <returns>A bool, if the item is currently in the sale.</returns>
         public static bool KSCheckIfItemInSale(Item item, Guid saleId) =>
             item.SaleItem.Any(s => s.SaleId.Equals(saleId));
+
+        /// <summary>
+        /// Checks if the <see cref="Item"/> is in the list of <see cref="Sale"/>s
+        /// <br/>
+        /// <code>Example: KSCheckIfItemInSale(cartItem.Item, businessSales)</code>
+        /// </summary>
+        /// <remarks>Liam De Rivers</remarks>
+        /// <param name="item">The item to check.</param>
+        /// <param name="sales">The list of sales to check.</param>
+        /// <returns>The Guid of the sale, if the item is present in it. Otherwise new Guid()</returns>
+        public static Guid KSCheckIfItemInSale(Item item, List<Sale> sales)
+        {
+            foreach (var sale in sales.Where(sale => item.SaleItem.Any(s => s.SaleId.Equals(sale.SaleId))))
+            {
+                return sale.SaleId;
+            }
+
+            return new Guid();
+        }
     }
 }
