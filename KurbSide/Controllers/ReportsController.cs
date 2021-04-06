f3e044a66a3cf39d4f6d7125a285fe50b6ed3e1e
Mark Moerkerken <markjamesmoerkerken@gmail.com>
2021-04-06 16:13:42 -0400

    Webside Reports
    Reports for
    Removed item
    Available Item
    All Items

--- KurbSide/Controllers/ReportsController.cs
+++ KurbSide/Controllers/ReportsController.cs
@@ -75,10 +75,64 @@ namespace KurbSide.Controllers
                 .Where(b => b.AspNetId.Equals(user.Id))
                 .FirstOrDefaultAsync();
 
-            var items = await _context.Item.Where(b => b.BusinessId.Equals(business.BusinessId)).ToListAsync();
+            
+            var items = await _context.Item
+                .Where(b => b.BusinessId.Equals(business.BusinessId))
+                .ToListAsync();
             
             return View(items);
+        }
+        
+        public async Task<IActionResult> ViewRemovedItemsReport()
+        {
+            //Check that the accessing user is a business type account
+            var user = await KSCurrentUser.KSGetCurrentUserAsync(_userManager, HttpContext);
+            var accountType = await KSCurrentUser.KSGetAccountType(_context, _userManager, HttpContext);
 
+            //If the currently logged in user is not a business they can not access business controllers.
+            if (accountType != KSCurrentUser.AccountType.BUSINESS)
+            {
+                TempData["sysMessage"] = "Error: You're not signed in as a business.";
+                return RedirectToAction("Index", "Home");
+            }
+
+            var business = await _context.Business
+                .Where(b => b.AspNetId.Equals(user.Id))
+                .FirstOrDefaultAsync();
+
+            
+            var items = await _context.Item
+                .Where(b => b.BusinessId.Equals(business.BusinessId))
+                .Where(i => i.Removed==true)
+                .ToListAsync();
+            
+            return View(items);
+        }
+        
+        public async Task<IActionResult> ViewAvailableItemsReport()
+        {
+            //Check that the accessing user is a business type account
+            var user = await KSCurrentUser.KSGetCurrentUserAsync(_userManager, HttpContext);
+            var accountType = await KSCurrentUser.KSGetAccountType(_context, _userManager, HttpContext);
+
+            //If the currently logged in user is not a business they can not access business controllers.
+            if (accountType != KSCurrentUser.AccountType.BUSINESS)
+            {
+                TempData["sysMessage"] = "Error: You're not signed in as a business.";
+                return RedirectToAction("Index", "Home");
+            }
+
+            var business = await _context.Business
+                .Where(b => b.AspNetId.Equals(user.Id))
+                .FirstOrDefaultAsync();
+
+            
+            var items = await _context.Item
+                .Where(b => b.BusinessId.Equals(business.BusinessId))
+                .Where(i => i.Removed==false)
+                .ToListAsync();
+            
+            return View(items);
         }
 
     }
