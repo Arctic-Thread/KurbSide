a3241730d828d55b672a5e89ff6e9787068b6643
Mark Moerkerken <markjamesmoerkerken@gmail.com>
2021-04-06 14:23:36 -0400

    Report Landing page added
    Report index page added shows what reports they can make (WIP)

---
+++ KurbSide/Controllers/ReportsController.cs
@@ -0,0 +1,62 @@
+﻿using KurbSide.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KurbSide.Service;
+using KurbSide.Utilities;
+
+
+namespace KurbSide.Controllers
+{
+    /// <summary>
+    /// Controller for Reports 
+    /// </summary>
+    [Authorize]
+    public class ReportsController : Controller
+    {
+        private readonly KSContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<Order> _logger;
+
+        public ReportsController(KSContext context, UserManager<IdentityUser> userManager, ILogger<Order> logger)
+        {
+            _context = context;
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Shows all the reports you can have
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> ViewAllReportList()
+        {
+            //Check that the accessing user is a business type account
+            var user = await KSCurrentUser.KSGetCurrentUserAsync(_userManager, HttpContext);
+            var accountType = await KSCurrentUser.KSGetAccountType(_context, _userManager, HttpContext);
+
+            //If the currently logged in user is not a business they can not access business controllers.
+            if (accountType != KSCurrentUser.AccountType.BUSINESS)
+            {
+                TempData["sysMessage"] = "Error: You're not signed in as a business.";
+                return RedirectToAction("Index", "Home");
+            }
+
+            var business = await _context.Business
+                .Where(b => b.AspNetId.Equals(user.Id))
+                .FirstOrDefaultAsync();
+
+            return View();
+
+        }
+
+    }
+}
