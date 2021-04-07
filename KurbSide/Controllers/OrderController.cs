22deb7b1ea76f040a5e16cadcde85e5b5deb10a1
Liam De Rivers <lderivers@gmail.com>
2021-04-07 17:15:47 -0400

    Prettier checkout

--- KurbSide/Controllers/OrderController.cs
+++ KurbSide/Controllers/OrderController.cs
@@ -296,6 +296,7 @@ namespace KurbSide.Controllers
                 }
             }
 
+            ViewData["sales"] = businessSales;
             ViewData["discountTotal"] = discountTotal;
             return View(cart);
         }
