9748d25183c7c70bb796743f05f873b4836e4079
Liam De Rivers <lderivers@gmail.com>
2021-04-10 18:53:00 -0400

    Tweaked report controller.
    Removed redundant code.
    Changed logic for report generation. You don't provide the business ID, it is retrieved from currently logged in business.

--- KurbSide/Controllers/ReportsController.cs
+++ KurbSide/Controllers/ReportsController.cs
@@ -1,24 +1,18 @@
 ﻿using KurbSide.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using KurbSide.Service;
 using KurbSide.Utilities;
 using Syncfusion.Pdf;
 using Syncfusion.Pdf.Graphics;
 using Syncfusion.Drawing;
 using System.IO;
-using System.Reflection.Metadata;
 using Syncfusion.Pdf.Grid;
-
 
 namespace KurbSide.Controllers
 {
@@ -30,23 +24,19 @@ namespace KurbSide.Controllers
     {
         private readonly KSContext _context;
         private readonly UserManager<IdentityUser> _userManager;
-        private readonly ILogger<Order> _logger;
 
-        public ReportsController(KSContext context, UserManager<IdentityUser> userManager, ILogger<Order> logger)
+        public ReportsController(KSContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
             _userManager = userManager;
-            _logger = logger;
         }
 
         /// <summary>
-        /// Shows all the reports you can have
+        /// Displays the report listing page.
         /// </summary>
-        /// <returns></returns>
         public async Task<IActionResult> ViewAllReportList()
         {
             //Check that the accessing user is a business type account
-            var user = await KSCurrentUser.KSGetCurrentUserAsync(_userManager, HttpContext);
             var accountType = await KSCurrentUser.KSGetAccountType(_context, _userManager, HttpContext);
 
             //If the currently logged in user is not a business they can not access business controllers.
@@ -56,20 +46,12 @@ namespace KurbSide.Controllers
                 return RedirectToAction("Index", "Home");
             }
 
-            var business = await _context.Business
-                .Where(b => b.AspNetId.Equals(user.Id))
-                .FirstOrDefaultAsync();
-
             return View();
-
         }
 
-        #region ItemReports
-
         /// <summary>
-        /// Shows a report for all the items
+        /// Displays the All Items Report page.
         /// </summary>
-        /// <returns></returns>
         public async Task<IActionResult> ViewAllItemsReport()
         {
             //Check that the accessing user is a business type account
@@ -87,22 +69,18 @@ namespace KurbSide.Controllers
                 .Where(b => b.AspNetId.Equals(user.Id))
                 .FirstOrDefaultAsync();
 
-            TempData["businessId"] = business.BusinessId;
-            
             //Gets all the items
             var items = await _context.Item
                 .Where(b => b.BusinessId.Equals(business.BusinessId))
                 .ToListAsync();
 
-            
-            
+            TempData["businessId"] = business.BusinessId;
             return View(items);
         }
-        
+
         /// <summary>
-        /// Shows report for the items that have been removed from the catalogue 
+        /// Displays the All Removed Items Report. 
         /// </summary>
-        /// <returns></returns>
         public async Task<IActionResult> ViewRemovedItemsReport()
         {
             //Check that the accessing user is a business type account
@@ -119,22 +97,20 @@ namespace KurbSide.Controllers
             var business = await _context.Business
                 .Where(b => b.AspNetId.Equals(user.Id))
                 .FirstOrDefaultAsync();
-            
-            TempData["businessId"] = business.BusinessId;
-            
+
             //Gets removed items
             var items = await _context.Item
                 .Where(b => b.BusinessId.Equals(business.BusinessId))
-                .Where(i => i.Removed==true)
+                .Where(i => i.Removed == true)
                 .ToListAsync();
-            
+
+            TempData["businessId"] = business.BusinessId;
             return View(items);
         }
-        
+
         /// <summary>
-        /// shows report for the item that are currently for sale
+        /// Displays the All Available Items Report.
         /// </summary>
-        /// <returns></returns>
         public async Task<IActionResult> ViewAvailableItemsReport()
         {
             //Check that the accessing user is a business type account
@@ -151,115 +127,127 @@ namespace KurbSide.Controllers
             var business = await _context.Business
                 .Where(b => b.AspNetId.Equals(user.Id))
                 .FirstOrDefaultAsync();
-            
-            TempData["businessId"] = business.BusinessId;
+
             //Get for sale items
             var items = await _context.Item
                 .Where(b => b.BusinessId.Equals(business.BusinessId))
-                .Where(i => i.Removed==false)
+                .Where(i => i.Removed == false)
                 .ToListAsync();
-            
+
+            TempData["businessId"] = business.BusinessId;
             return View(items);
         }
-        
+
         /// <summary>
-        /// The function for downloading report Pdf's
+        /// Downloads a PDF version of the specified report.
         /// </summary>
-        /// <param name="pdfName"></param>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        public async Task<FileResult> CreateItemsReportsPdf(string pdfName, Guid businessId)
+        /// <param name="pdfName">The name of the report</param>
+        public async Task<FileResult> CreateItemsReportsPdf(string pdfName)
         {
+            //Check that the accessing user is a business type account
+            var user = await KSCurrentUser.KSGetCurrentUserAsync(_userManager, HttpContext);
+            var accountType = await KSCurrentUser.KSGetAccountType(_context, _userManager, HttpContext);
+
+            //If the currently logged in user is not a business they can not access business controllers.
+            if (accountType != KSCurrentUser.AccountType.BUSINESS)
+                return null;
+
+            var business = await _context.Business
+                .Where(b => b.AspNetId.Equals(user.Id))
+                .FirstOrDefaultAsync();
+
             string fileName = "";
-            
+
             MemoryStream workStream = new MemoryStream();
-            DateTime currentDate= DateTime.Now;
-            
+
             //Create a new document
             PdfDocument itemsReportDocument = new PdfDocument();
-            
+
             //Adding a page to the itemsReportDocument 
             PdfPage page = itemsReportDocument.Pages.Add();
-            
+
             //Pdf Graphics for the page
             PdfGraphics graphics = page.Graphics;
-            
+
             //Pdf grid for the table
             PdfGrid pdfGrid = new PdfGrid();
-            
+
             //Set the standard font
             PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
 
-            //Downloads report for all the items
-            if (pdfName == "AllItemsReport")
+            switch (pdfName)
             {
-                 var itemsList =  await _context.Item
-                    .Where(b => b.BusinessId.Equals(businessId))
-                    .Select(i=> new{i.ItemName, i.Price,i.Category,i.Sku,i.Upc,i.Removed })
-                    .ToListAsync();
-                 
-                 //Make the list to IEnumerable
-                 IEnumerable<object> itemTable = itemsList;
-                 //Assign to data source
-                 pdfGrid.DataSource = itemTable;
-                 fileName = "All Items Report";
-                
+                //Downloads report for all the items
+                case "AllItemsReport":
+                {
+                    var itemsList = await _context.Item
+                        .Where(b => b.BusinessId.Equals(business.BusinessId))
+                        .Select(i => new {i.ItemName, i.Price, i.Category, i.Sku, i.Upc, i.Removed})
+                        .ToListAsync();
+
+                    //Make the list to IEnumerable
+                    IEnumerable<object> itemTable = itemsList;
+                    //Assign to data source
+                    pdfGrid.DataSource = itemTable;
+                    fileName = "All Items Report";
+                    break;
+                }
+                //DownLoads report for all the Available items 
+                case "AvailableItemsReport":
+                {
+                    var itemsList = await _context.Item
+                        .Where(b => b.BusinessId.Equals(business.BusinessId))
+                        .Where(i => i.Removed == false)
+                        .Select(i => new {i.ItemName, i.Price, i.Category, i.Sku, i.Upc, i.Removed})
+                        .ToListAsync();
+
+                    //Make the list to IEnumerable
+                    IEnumerable<object> itemTable = itemsList;
+                    //Assign to data source
+                    pdfGrid.DataSource = itemTable;
+                    fileName = "Available Items Report";
+                    break;
+                }
+                //Downloads report for all the removed items
+                case "RemovedItemsReport":
+                {
+                    var itemsList = await _context.Item
+                        .Where(b => b.BusinessId.Equals(business.BusinessId))
+                        .Where(i => i.Removed == true)
+                        .Select(i => new {i.ItemName, i.Price, i.Category, i.Sku, i.Upc, i.Removed})
+                        .ToListAsync();
+
+                    //Make the list to IEnumerable
+                    IEnumerable<object> itemTable = itemsList;
+                    //Assign to data source
+                    pdfGrid.DataSource = itemTable;
+                    fileName = "Removed Items Report";
+                    break;
+                }
             }
-            //DownLoads report for all the Available items 
-            else if (pdfName == "AvailableItemsReport")
-            {
-                var itemsList =  await _context.Item
-                    .Where(b => b.BusinessId.Equals(businessId))
-                    .Where(i=> i.Removed==false)
-                    .Select(i=> new{i.ItemName, i.Price,i.Category,i.Sku,i.Upc,i.Removed })
-                    .ToListAsync();
-                
-                //Make the list to IEnumerable
-                IEnumerable<object> itemTable = itemsList;
-                //Assign to data source
-                pdfGrid.DataSource = itemTable;
-                fileName = "Available Items Report";
-                
-            }
-            //Downloads report for all the removed items
-            else if (pdfName=="RemovedItemsReport")
-            {
-                var itemsList =  await _context.Item
-                    .Where(b => b.BusinessId.Equals(businessId))
-                    .Where(i => i.Removed==true)
-                    .Select(i=> new{i.ItemName, i.Price,i.Category,i.Sku,i.Upc,i.Removed })
-                    .ToListAsync();
-                
-                //Make the list to IEnumerable
-                IEnumerable<object> itemTable = itemsList;
-                //Assign to data source
-                pdfGrid.DataSource = itemTable;
-                fileName = "Removed Items Report";
-            }
-            
+
             //Styling the grid
             pdfGrid.Style.Font = new PdfStandardFont(PdfFontFamily.Helvetica, 8);
             pdfGrid.Style.CellPadding = new PdfPaddings(4, 4, 4, 4);
-            
+
             //Draw the table to a pdf page
-            graphics.DrawString(fileName, font,PdfBrushes.Black, new PointF(10,0));
-            pdfGrid.Draw(page, new Syncfusion.Drawing.PointF(10, 30));
-            
+            graphics.DrawString(fileName, font, PdfBrushes.Black, new PointF(10, 0));
+            pdfGrid.Draw(page, new PointF(10, 30));
+
             //Saves the Pdf to the stream
             itemsReportDocument.Save(workStream);
             workStream.Position = 0;
-            
+
             //close the stream
             itemsReportDocument.Close(true);
-            
-            //Content type for pdf as well as file name
-            string type = "application/pdf";
 
-            fileName = fileName + ".pdf";
+            //Content type for pdf as well as file name
+            const string type = "application/pdf";
+
+            string currentDate = DateTime.Today.ToString("d");
+            fileName = $"KurbSide-{fileName}-{currentDate}.pdf";
 
             return File(workStream, type, fileName);
         }
-        
-        #endregion
     }
 }
