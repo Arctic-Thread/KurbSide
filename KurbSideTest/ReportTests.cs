651c6f7aa9a959cf4417379c5dc656e52c2673f0
Mark Moerkerken <markjamesmoerkerken@gmail.com>
2021-04-10 16:33:57 -0400

    UnitTest Reports
    All items unit test

--- KurbSideTest/ReportTests.cs
+++ KurbSideTest/ReportTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using NUnit.Framework;
 using OpenQA.Selenium;
 
@@ -9,19 +10,44 @@ namespace KurbSideTest
     class ReportTests : BaseTest
     {
         /// <summary>
-        /// UC15 - Business Add Item
+        /// UC21- Business views Reports
         /// Tests error message generation when adding an item with invalid details.
+        /// Note: To run this test you may have to modify the download path
+        /// Note: For Best results ensure the download path is clear on you device
         /// </summary>
         [Test]
         [Order(1)]
-        public void UC15_Items_AddItem_InvalidDetails_ShouldFail()
+        public void UC21_Reports_DownLoadReport_AllItems_ShouldPass()
         {
+            //strings needed for the test
+            string dashboard = "Business Dashboard - KurbSide";
+            string reportsListing = "Business Report Listing - KurbSide";
+            string allItemsReport = "All Items Report - KurbSide";
+            string downLoadPath = @"C:\Users\User\Downloads\All Items Report.pdf";//You may need to modify based on the file location
+            string fileName = "All Items Report.pdf";
+            
+            
+            //logins to the business account
             KSUnitTestLogin(AccountType.BUSINESS);
             
+            //From the dashboard navigates to report listings
+            KSTitleContains(dashboard);
+            System.Threading.Thread.Sleep(200);
+            KSClick("dashboard-reports");
             
+            //navigates to all items reports
+            KSTitleContains(reportsListing);
+            KSClick("reports-allItems");
             
+            //Waits till intended page displays
+            KSTitleContains(allItemsReport);
+            KSClick("btnDownloadGameListReport");
             
-            Assert.AreEqual(addFailed, true);//checks to make sure that there are items in the list
+            //Checks if the file was downloaded
+
+            System.Threading.Thread.Sleep(10000);
+            
+            Assert.IsTrue(File.Exists(downLoadPath));//checks to make sure that there are items in the list
         }
         
         
