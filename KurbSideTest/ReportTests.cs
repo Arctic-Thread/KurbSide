0a62cbd605c2c100c230591570f37f07500126c7
Mark Moerkerken <markjamesmoerkerken@gmail.com>
2021-04-10 16:39:39 -0400

    Unit test reports
    Unit test for available items report

--- KurbSideTest/ReportTests.cs
+++ KurbSideTest/ReportTests.cs
@@ -24,8 +24,6 @@ namespace KurbSideTest
             string reportsListing = "Business Report Listing - KurbSide";
             string allItemsReport = "All Items Report - KurbSide";
             string downLoadPath = @"C:\Users\User\Downloads\All Items Report.pdf";//You may need to modify based on the file location
-            string fileName = "All Items Report.pdf";
-            
             
             //logins to the business account
             KSUnitTestLogin(AccountType.BUSINESS);
@@ -37,6 +35,7 @@ namespace KurbSideTest
             
             //navigates to all items reports
             KSTitleContains(reportsListing);
+            System.Threading.Thread.Sleep(200);
             KSClick("reports-allItems");
             
             //Waits till intended page displays
@@ -49,6 +48,48 @@ namespace KurbSideTest
             
             Assert.IsTrue(File.Exists(downLoadPath));//checks to make sure that there are items in the list
         }
+        
+        /// <summary>
+        /// UC21- Business views Reports
+        /// Tests error message generation when adding an item with invalid details.
+        /// Note: To run this test you may have to modify the download path
+        /// Note: For Best results ensure the download path is clear on you device
+        /// </summary>
+        [Test]
+        [Order(2)]
+        public void UC21_Reports_DownLoadReport_AvailableItems_ShouldPass()
+        {
+            //strings needed for the test
+            string dashboard = "Business Dashboard - KurbSide";
+            string reportsListing = "Business Report Listing - KurbSide";
+            string allItemsReport = "Available Items Report - KurbSide";
+            string downLoadPath = @"C:\Users\User\Downloads\Available Items Report.pdf";//You may need to modify based on the file location
+            
+            //logins to the business account
+            KSUnitTestLogin(AccountType.BUSINESS);
+            
+            //From the dashboard navigates to report listings
+            KSTitleContains(dashboard);
+            System.Threading.Thread.Sleep(200);
+            KSClick("dashboard-reports");
+            
+            //navigates to all items reports
+            KSTitleContains(reportsListing);
+            System.Threading.Thread.Sleep(200);
+            KSClick("reports-availableItems");
+            
+            //Waits till intended page displays
+            KSTitleContains(allItemsReport);
+            KSClick("btnDownloadGameListReport");
+            
+            //Checks if the file was downloaded
+
+            System.Threading.Thread.Sleep(10000);
+            
+            Assert.IsTrue(File.Exists(downLoadPath));//checks to make sure that there are items in the list
+        }
+        
+        
         
         
     }    
